ed4bda28c0d58e0e17b10ea2089a4aec65821fc1
Skylar Panuska <skylar.panuska@gmail.com>
2015-05-08 17:55:29 -0400

    prints a grid when initializing a game

--- lib/board.rb
+++ lib/board.rb
@@ -8,8 +8,20 @@ class Board
 	end
 
 	def build_grid
-		@grid = [[[1], [2], [3], "\n"], [4,5,6, "\n"], [7,8,9]]
-		puts @grid
+		@grid = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+		# @grid[0][0] = 1
+		# @grid[0][1] = 2
+		# @grid[0][2] = 3
+		# @grid[1][0] = 4
+		# @grid[1][1] = 5
+		# @grid[1][2] = 6
+		# @grid[2][0] = 7
+		# @grid[2][1] = 8
+		# @grid[2][2] = 9
+		
+		p @grid.slice(0)
+		p @grid.slice(1)
+		p @grid.slice(2)
 	end
 
 end
